f2bdda20139319335682aca1bee9f5d43f859d5e
MichaelKramerGuitar <michael.kramer.guitar@gmail.com>
2022-05-27 10:19:18 -0400

    progress on calc

--- src/modules/main.js
+++ src/modules/main.js
@@ -20,9 +20,50 @@ window.addEventListener('load', (event) => {
     alert('Welcome to Calculator!\nWe\'re all loaded up and ready to begin...');
     // prompt for name
     let name = window.prompt("But first, what's your name?")
-    let welcome = document.getElementById("welcome-message")
+    let welcome = document.getElementById("welcome-message");
+    // let welcome = document.querySelector("welcome");
     welcome.innerHTML = "Welcome, " + name + "!";
     // display name on page
     document.body.appendChild(welcome);
 });
 
+// calculator
+let calc = new Calculator();
+
+// input elements
+const input = document.getElementById('input');
+
+// display elements
+const current = document.getElementById('current');
+const operator = document.getElementById('operator');
+const previous = document.getElementById('previous');
+
+const operations = document.getElementsByClassName('operation');
+
+current.innerHTML = "0"; // initialize display
+
+input.addEventListener("click", (event) => {
+    console.log(event.target)
+    if(event.target.id in operations) {
+        operator.innerHTML = event.target.value;
+    }
+    else if (!(event.target.id in operations)) {
+        current.innerHTML = event.target.value;
+    }
+    else { // get proper operator
+        alert(operator.innerHTML)
+        // if(current.innerHTML === "0"){
+        //     calc.execute(new Add(parseFloat(current.innerHTML)));
+        // }
+        previous.innerHTML = calc.getValue();
+        if (operator.innerHTML === "/") {
+            calc.execute(new Divide(parseFloat(current.innerHTML)))
+            current.innerHTML = calc.getValue();
+        }
+        else if(operator.innerHTML === "+") {
+            calc.execute(new Add(parseFloat(current.innerHTML)))
+            current.innerHTML = calc.getValue();
+        }
+    }
+
+})
